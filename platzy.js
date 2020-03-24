3c2f6a95ba6049ad8eccc8d116f257d7ee2ceb26
Aristides Alonso <aristidescastilla@hotmail.com>
2020-03-23 19:11:33 -0600

    Generando numeros aleatorios

--- platzy.js
+++ platzy.js
@@ -1,1 +1,16 @@
-alert("asdfasdf");
+var z
+
+for(var i=0; i<10; i++)
+{
+  z = aleatorio(0, 5);
+  document.write(z +  ", ");
+}
+
+
+
+function aleatorio(min, maxi)
+{
+  var resultado;
+  resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+  return resultado
+}
